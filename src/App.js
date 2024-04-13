262f8e96c18c9459ab007a8f0ae2b4a562bdf205
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2024-04-13 21:28:10 +0700

    aa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWord from './componeds/HelloWord';
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [arrayData, setArrayData] = useState([])
+  const [inputValue, seTInputValue] = useState('')
+  const handleInputChange = (e) => {
+    seTInputValue(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <input 
+        type="text"
+        placeholder="Enter your name"
+        value={inputValue}
+        onChange={handleInputChange}
+        />
+      </div>
+      
+    
     </div>
+    
   );
 }
 
---
+++ src/componeds/HelloWord.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import'../App.css'
+const HelloWord = () => {
+  return (
+    <div className='custom-hello-word'>HelloWord</div>
+  )
+}
+export default HelloWord
